a137a196259563ce91440d546586bc34951541e8
Mayank Singh <51783955+mayank-siingh@users.noreply.github.com>
2019-11-10 16:33:53 +0530

    Add files via upload

---
+++ Jupyter-Notebook.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is for the "Capstone Project" by IBM course on Coursera.
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
